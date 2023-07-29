2bb7f5d91eb93d6a698b3809232a7ef10d6704b8
wangtao <wangtao970503@foxmail.com>
2023-07-29 16:26:58 +0800

    抽取公共方法

--- redis-common/src/main/java/com/wangtao/redis/common/RedisConfig.java
+++ redis-common/src/main/java/com/wangtao/redis/common/RedisConfig.java
@@ -1,31 +1,11 @@
 package com.wangtao.redis.common;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import com.wangtao.redis.common.util.RedisSerializerUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
-
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 /**
  * @author wangtao
@@ -34,65 +14,15 @@ import java.time.format.DateTimeFormatter;
 @Configuration
 public class RedisConfig {
 
-    private static final String STANDARD_PATTERN = "yyyy-MM-dd HH:mm:ss";
-    private static final String DATE_PATTERN = "yyyy-MM-dd";
-    private static final String TIME_PATTERN = "HH:mm:ss";
-
     @Bean
     public <V> RedisTemplate<String, V> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, V> redisTemplate = new RedisTemplate<>();
-        RedisSerializer<Object> valueRedisSerializer = initValueRedisSerializer();
+        RedisSerializer<Object> valueRedisSerializer = RedisSerializerUtils.create();
         redisTemplate.setKeySerializer(RedisSerializer.string());
         redisTemplate.setValueSerializer(valueRedisSerializer);
         redisTemplate.setHashKeySerializer(RedisSerializer.string());
         redisTemplate.setHashValueSerializer(valueRedisSerializer);
         redisTemplate.setConnectionFactory(connectionFactory);
         return redisTemplate;
-    }
-
-    private RedisSerializer<Object> initValueRedisSerializer() {
-        Jackson2JsonRedisSerializer<Object> valueRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        ObjectMapper objectMapper = new ObjectMapper();
-        // objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
-        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
-                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
-
-        // 设置java.util.Date时间类的序列化以及反序列化的格式
-        objectMapper.setDateFormat(new SimpleDateFormat(STANDARD_PATTERN));
-
-        // 设置BigDecimal
-        objectMapper.enable(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN);
-
-        // 初始化JavaTimeModule
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
-
-        //处理LocalDateTime
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(STANDARD_PATTERN);
-        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(dateTimeFormatter));
-        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(dateTimeFormatter));
-
-        //处理LocalDate
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
-        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(dateFormatter));
-        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(dateFormatter));
-
-        //处理LocalTime
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern(TIME_PATTERN);
-        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(timeFormatter));
-        javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(timeFormatter));
-
-        //注册时间模块, 支持支持jsr310, 即新的时间类(java.time包下的时间类)
-        objectMapper.registerModule(javaTimeModule);
-
-        // 包含所有字段
-        objectMapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
-
-        // 在序列化一个空对象时时不抛出异常
-        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-
-        // 忽略反序列化时在json字符串中存在, 但在java对象中不存在的属性
-        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        valueRedisSerializer.setObjectMapper(objectMapper);
-        return valueRedisSerializer;
     }
 }
